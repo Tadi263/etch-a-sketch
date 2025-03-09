de93cad47439d86ac2e436f83418286d418a6402
Tadi Mhizha <151214925+Tadi263@users.noreply.github.com>
2025-03-09 16:15:35 +0000

    added rainbow functionality

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const container = document.querySelector("#grid-container");
 
 //get box quantity
-
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = `${slider.value} x ${slider.value}`; // Display the default slider value
 let quantity = slider.value;
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   quantity = this.value;
-  output.innerHTML = this.value;
+  output.innerHTML = `${this.value} x ${this.value}`;
 }
 
 
@@ -37,13 +36,8 @@ slider.oninput = function() {
   slider.addEventListener("change", function() {
     // Get the collection of boxes
     let boxes = document.getElementsByClassName('box');
-  
-    // Convert HTMLCollection to an array
     const boxesArray = Array.from(boxes);
-  
-    // Now you can use forEach
     boxesArray.forEach((div) => div.remove());
-  
       // recreate boxes
       createBoxes(quantity * quantity);
   }, false);
@@ -66,9 +60,11 @@ document.getElementById("pickcolor").onchange = function() {
     "mouseover",
     (event) => {
       // highlight the mouseover target
-      if (event.target !== container && eraserstate == false) {
+      if (event.target !== container && eraserstate == false && rainbowstate == false) {
         event.target.style.backgroundColor = pickedcolor;}
-      else if (event.target !== container && eraserstate == true){
+      else if (event.target !== container && eraserstate == false && rainbowstate == true){
+        event.target.style.backgroundColor = getRandomColor();}
+      else if (event.target !== container && eraserstate == true && rainbowstate == false){
           event.target.style.backgroundColor = "white";}
     },
     false,)
@@ -76,23 +72,34 @@ document.getElementById("pickcolor").onchange = function() {
 
 
 //eraser functionality
-const eraser = document.querySelector("#eraser");
-eraserstate = false;
-eraser.addEventListener("click", () => {
+  const eraser = document.querySelector("#eraser");
+  eraserstate = false;
+  eraser.addEventListener("click", () => {
   eraserstate = !eraserstate;
-  if (eraserstate == true){
-    eraser.style = "background-color: grey; border: solid, 1px, black;";
-  }
-  else {
-    eraser.style = "background-color: lightgrey; border: solid, 1px, grey;";
-  }
+  rainbowstate = false;
+  rainbow.classList.remove('active');
+  eraser.classList.toggle('active');
   console.log(eraserstate);})
 
-  //eraser functionality
- 
+  //rainbow functionality
+  const rainbow = document.querySelector("#rainbow");
+  rainbowstate = false;
+  rainbow.addEventListener("click", () => {
+  rainbowstate = !rainbowstate;
+  eraserstate = false;
+  eraser.classList.remove('active');
+  rainbow.classList.toggle('active');
+  console.log(rainbowstate);})
+
+  function getRandomColor() {
+    return `rgb(${Math.floor(Math.random() * 256)}, 
+                ${Math.floor(Math.random() * 256)}, 
+                ${Math.floor(Math.random() * 256)})`;
+}
+
+  //clear functionality
   const clear = document.querySelector("#clear");
   clear.addEventListener("click", () => {
-    for(let i = 0; i < boxes.length; i++){
-      boxes[i].style = `background-color: white; height: ${400 / quantity}px; width:  ${400 / quantity}px; border: solid 1px grey;`;}
+    updateBoxStyles();
   })
 
